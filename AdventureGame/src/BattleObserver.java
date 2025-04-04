f9545fd6220bedb2f4cc2b96bb722a9c86fccec6
Dhairya <sharmadhairya024@gmail.com>
2025-04-04 09:20:13 +0530

    Delete AdventureGame/src directory

--- AdventureGame/src/BattleObserver.java
+++
@@ -1,8 +0,0 @@
-// Class demonstrating the Observer pattern through interfaces
-public interface BattleObserver {
-    void onBattleStart(Creature player, Creature opponent);
-
-    void onBattleEnd(Creature winner, Creature loser);
-
-    void onAttackExecuted(Creature attacker, Attack attack, Creature defender, int damage);
-}
